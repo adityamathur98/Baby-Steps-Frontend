d4d5a1de260999c1ec5daea7555d93990b23154f
chitransh mathur <chitransh.code.992@gmail.com>
2025-02-26 01:28:23 +0530

    Made some changes on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Allows Vite to bind to all network interfaces (for external access)
+    port: process.env.PORT || 3000, // Use the PORT environment variable on Render
+  },
 });
